b1ef471082fc6c3611b2e0a24f0e6b700479e3e3
Dameon Brown <axel12gt@live.com>
2019-06-18 14:19:32 -0400

    turned it into a function call

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,23 +1,21 @@
-var queryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?...&location=32817&app_key=DnChmPW9wRV8NBcd"
-console.log(queryURL)
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    let results =  JSON.parse(response)
-     console.log("results: " + results.events.event[0].olson_path)
-
-    // forwards all the information to info function
-    info(results)
-}) 
+function ajx() {
+    var location = "32817"
+    var queryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?...&location=" + location + "&app_key=DnChmPW9wRV8NBcd"
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        let results = JSON.parse(response)
+        // forwards all the information to info function
+        info(results)
+    })
     //function where we will go through our info object and gather the resources to load to the DOM
-function info(results){
-    var dat
-    console.log("is working")
-    console.log(results.events.event.length)
-        for(var i = 0; i < results.events.event.length; i++){
-            
-            console.log(i)
+    function info(results) {
+        var dat
+        console.log("is working")
+        console.log(results.events.event.length)
+        for (var i = 0; i < results.events.event.length; i++) {
             // creates HTML li element
             li = $("<li>")
             // creates HTML div with class collapsible header
@@ -32,19 +30,21 @@ function info(results){
             dat = results.events.event[i].start_time
             spanH.append(moment(dat).format("MMM Do YY"))
             // appends object.spanH[i] to divH
-            divH.append(h5,spanH)
+            divH.append(h5, spanH)
             // appends divB
             divB = $("<div class='collapsible-body'>")
             // creates span
             spanB = $("<span>")
             // appends obj.venue obj.address obj.url
-            spanB.append(results.events.event[i].venue_name, "<br>", results.events.event[i].venue_address, "<br>", results.events.event[i].url)
+            spanB.append(results.events.event[i].venue_name, "<br>", results.events.event[i].venue_address, "<br>", "<br>",
+                results.events.event[i].description, "<br>", "<br>", "<a target='_blank' href=" + results.events.event[i].url + ">" + results.events.event[i].url + "</a>")
             // appends spanB to divB
-            divB.append(spanB)  
+            divB.append(spanB)
             // appends divH and divB
-            li.append(divH,divB)
-            $("<ul class='collapsible popout'>").append(li)
+            li.append(divH, divB)
+            $(".collapsible").append(li)
         }
+    }
+}
 
-        
-        }
+ajx()
